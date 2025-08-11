13580c4fe130ce0fc716f41b4fe1b39ee8e8ae90
tanuj <you@3example3.com>
2025-08-12 00:20:18 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import { useNavigate } from 'react-router-dom';
 
 function App() {
 
-  const nav = useNavigate();
 
   const [id, setId] = useState();
   const [name, setname] = useState();
   const [post, setPost] = useState();
   const [readData, setReadData] = useState();
+
+  const [editShow, setEditShow] = useState();
+
+  const [editid, setEditId] = useState();
+  const [editname, setEditname] = useState();
+  const [editpost, setEditPost] = useState();
+
 
   // create Data
   function handleSubmit(e) {
@@ -29,13 +34,22 @@ function App() {
   function handleEdit(e) {
     e.preventDefault();
 
+
+    let id = editid;
+    let name = editname;
+    let post = editpost;
+
+
     const data = { id, name, post };
 
-    fetch('http://localhost:8000/users/3', {
+    fetch('http://localhost:8000/users/'+editid, {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data)
     });
+
+    read();
+
   }
 
   // Delete Data
@@ -47,39 +61,57 @@ function App() {
       // nav("/index"); 
       read();
     })
-
   }
 
   // Read Data
+  const readAsign = async (id) => {
+    let res = await fetch('http://localhost:8000/users/' + id);
+    let data = await res.json();
+    setEditId(data['id']);
+    setEditname(data['name']);
+    setEditPost(data['post']);
+  }
+
+  // Read the id and Data
   const read = async () => {
     let res = await fetch('http://localhost:8000/users');
     let data = await res.json();
     setReadData(data);
   }
 
+
   useEffect(() => {
     read();
   }, []);
 
-
   return (
     <>
 
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder='Enter Id' value={id} onChange={(e) => setId(e.target.value)} />
-        <input type="text" name='name' placeholder='enter Name' value={name} onChange={(e) => setname(e.target.value)} />
-        <input type="text" name='post' placeholder='enter post' value={post} onChange={(e) => setPost(e.target.value)} />
-        <button type="submit">Add</button>
+      <form className='flex items-center justify-center gap-2' onSubmit={handleSubmit}>
+        <input className='border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500' type="text" placeholder='Enter Id' value={id} onChange={(e) => setId(e.target.value)} />
+        <input className='border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500' type="text" name='name' placeholder='enter Name' value={name} onChange={(e) => setname(e.target.value)} />
+        <input className='border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500' type="text" name='post' placeholder='enter post' value={post} onChange={(e) => setPost(e.target.value)} />
+        <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600" type="submit">Add</button>
       </form>
 
-      <form className='updata_form' onSubmit={handleEdit}>
-        <input type="text" placeholder='Enter Id' value={id} onChange={(e) => setId(e.target.value)} />
-        <input type="text" name='name' placeholder='enter Name' value={name} onChange={(e) => setname(e.target.value)} />
-        <input type="text" name='post' placeholder='enter post' value={post} onChange={(e) => setPost(e.target.value)} />
-        <button type="submit">Update</button>
-      </form>
 
-      <table className="min-w-full border border-gray-300 rounded-lg shadow-md">
+      {editShow && (
+
+        <form className='updata_form justify-center flex items-center  gap-2' onSubmit={handleEdit}>
+          <input className='border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500' 
+          type="text" placeholder='Enter Id' value={editid} onChange={(e) => setEditId(e.target.value)} />
+          
+          <input className='border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500' 
+          type="text" name='name' placeholder='enter Name' value={editname} onChange={(e) => setEditname(e.target.value)} />
+          
+          <input className='border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500' 
+          type="text" name='post' placeholder='enter post' value={editpost} onChange={(e) => setEditPost(e.target.value)} />
+          <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600" type="submit">Update</button>
+        </form>
+
+      )}
+
+      <table className="table_box border border-gray-300 rounded-lg shadow-md">
         <thead className="bg-gray-200">
           <tr>
             <th className="px-4 py-2 border border-gray-300">ID</th>
@@ -89,24 +121,24 @@ function App() {
           </tr>
         </thead>
         <tbody>
-
           {
-
             readData?.map((item, index) => (
               <>
-                <tr className="hover:bg-gray-100 transition duration-200">
+                <tr key={index} className="hover:bg-gray-100 transition duration-200">
                   <td className="px-4 py-2 border border-gray-300">{item.id}</td>
                   <td className="px-4 py-2 border border-gray-300">{item.name}</td>
                   <td className="px-4 py-2 border border-gray-300">{item.post}</td>
-                  <td className="px-4 py-2 border border-gray-300"><button>Edit</button><button onClick={() => handleDelete(item.id)} className='delete'>Delete</button></td>
+                  <td className="px-4 py-2 border border-gray-300">
+                    <button onClick={() => {setEditShow(true); readAsign(item.id);}} className='bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600'>Edit</button>
+                    <button onClick={() => handleDelete(item.id)} className='bg-red-500 ml-5 text-white px-4 py-2 rounded hover:bg-red-600'>Delete</button>
+                  </td>
                 </tr>
               </>
             ))
           }
-
         </tbody>
       </table>
-      
+
 
     </>
   )
